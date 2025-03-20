0b1d948494e1da09ca1f52107d68eecaad8884f4
Bryannarte <bnarte@filamer.edu.ph>
2025-03-20 17:19:27 +0800

    new version

---
+++ Getting Functional/function.js
@@ -0,0 +1,32 @@
+var dogName = "Rover";
+var dogweigth = 30;
+if (dogWeight > 20) {
+    console.log (dogName + " says WOOF WOOF");
+}   else { 
+    console.log (dogName + " says WOOF WOOF");
+}
+
+
+var dogName = "zafra";
+var dogweigth = 50;
+if (dogWeight > 20) {
+    console.log (dogName + " says WOOF WOOF");
+}   else { 
+    console.log (dogName + " says WOOF WOOF");
+}
+
+var dogName = "marc";
+var dogweigth = 15;
+if (dogWeight > 20) {
+    console.log (dogName + " says WOOF WOOF");
+}   else { 
+    console.log (dogName + " says WOOF WOOF");
+}
+
+var dogName = "aya";
+var dogweigth = 10;
+if (dogWeight > 20) {
+    console.log (dogName + " says WOOF WOOF");
+}   else { 
+    console.log (dogName + " says WOOF WOOF");
+}
